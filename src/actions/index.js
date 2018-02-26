15f9128838222ba8e9e3c5a80b33b630736a28ba
ZebGirouard <timzebgir@gmail.com>
2018-02-26 12:26:21 -0700

    Pushing up for sharing

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'af8289b261d52a99d52942fca83a3833';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
